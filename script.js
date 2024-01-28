73d8ac155706c23f442d44b9b6aa856e0778649e
RusLena <RusLena@users.noreply.github.com>
2024-01-28 11:30:48 +0000

    added bootstrap link, api key, created cards and added some style

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initial array of cities
+var cities = ["London", "Paris", "New York", "Barcelona"];
+var apiKey = "bd7f0edb536c03ac41654169d6834003";
+var queryURL = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={bd7f0edb536c03ac41654169d6834003}"
+function buildQueryURL() {
+
+}
