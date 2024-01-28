a283b9456e8edcea3870bab6eeafbf12d245d441
RusLena <RusLena@users.noreply.github.com>
2024-01-28 13:17:04 +0000

    added divs into containers to store weather data, added fetch function, and catch error because I have 401 error msg

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // Initial array of cities
-var cities = ["London", "Paris", "New York", "Barcelona"];
-var apiKey = "bd7f0edb536c03ac41654169d6834003";
-var queryURL = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={bd7f0edb536c03ac41654169d6834003}"
-function buildQueryURL() {
+var cities = [];
+//var APIKey = "172e14c8b1f9b91de3f6976630d9864a";
+//var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=" + APIKey;
 
-}
+
+
+// Fetch call to the OpenWeatherMap API
+fetch(queryURL)
+  .then(function (response) {
+    // Calling .json() to access the json data 
+    return response.json();
+  })
+  // Store all of the retrieved data 
+  .then(function (data) {
+
+    // Log the queryURL
+    console.log(data);
+// Log the resulting object
+    console.log(data);
+    
+})
+// APIKey error 401
+    .catch(function(error) {
+        console.error("Error fetching weather data:", error);
+    }); 
+
+   
